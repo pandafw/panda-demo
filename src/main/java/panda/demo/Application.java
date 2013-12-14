96243f2bb4bcf2c4fb0b2ce7f35bde710045cf46
Frank Wang <yf.frank.wang@outlook.com>
2013-12-14 16:30:02 +0900

    comment lucene usage

--- src/main/java/panda/demo/Application.java
+++ src/main/java/panda/demo/Application.java
@@ -23,41 +23,41 @@ import panda.demo.entity.SampleUnique;
  */
 public class Application extends ServletApplet {
 
-	private static File lucenePath;
-	private static Directory luceneDirectory;
+//	private static File lucenePath;
+//	private static Directory luceneDirectory;
 	
-	/**
-	 * @return the luceneDirectory
-	 */
-	public static Directory getLuceneDirectory() {
-		return luceneDirectory;
-	}
-
-	/**
-	 * @return the luceneIndexWriter
-	 */
-	public static IndexWriter getLuceneIndexWriter() throws Exception {
-		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_45);
-		IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_45, analyzer);
-		iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-
-		// Optional: for better indexing performance, if you
-		// are indexing many documents, increase the RAM
-		// buffer. But if you do this, increase the max heap
-		// size to the JVM (eg add -Xmx512m or -Xmx1g):
-		//
-		// iwc.setRAMBufferSizeMB(256.0);
-
-		IndexWriter writer = new IndexWriter(getLuceneDirectory(), iwc);
-		return writer;
-	}
+//	/**
+//	 * @return the luceneDirectory
+//	 */
+//	public static Directory getLuceneDirectory() {
+//		return luceneDirectory;
+//	}
+//
+//	/**
+//	 * @return the luceneIndexWriter
+//	 */
+//	public static IndexWriter getLuceneIndexWriter() throws Exception {
+//		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_45);
+//		IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_45, analyzer);
+//		iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+//
+//		// Optional: for better indexing performance, if you
+//		// are indexing many documents, increase the RAM
+//		// buffer. But if you do this, increase the max heap
+//		// size to the JVM (eg add -Xmx512m or -Xmx1g):
+//		//
+//		// iwc.setRAMBufferSizeMB(256.0);
+//
+//		IndexWriter writer = new IndexWriter(getLuceneDirectory(), iwc);
+//		return writer;
+//	}
 
 	@Override
 	protected void initialize() throws Exception {
 		super.initialize();
 		
-		lucenePath = new File(Application.i().getPropertyAsPath("lucene.directory", "~/WEB-INF/lucene"));
-		luceneDirectory = FSDirectory.open(lucenePath);
+//		lucenePath = new File(Application.i().getPropertyAsPath("lucene.directory", "~/WEB-INF/lucene"));
+//		luceneDirectory = FSDirectory.open(lucenePath);
 		
 		//initialize entities
 		getDaoClient().getEntity(SampleDate.class);
