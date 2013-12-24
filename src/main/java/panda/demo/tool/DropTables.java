1f9606030c35a6cf31a94a2862dd3b02d4dcac9b
Frank Wang <yf.frank.wang@outlook.com>
2013-12-24 22:50:53 +0900

    remove unused code

--- src/main/java/panda/demo/tool/DropTables.java
+++ src/main/java/panda/demo/tool/DropTables.java
@@ -1,10 +1,5 @@
 package panda.demo.tool;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.cli.CommandLine;
-
 import panda.aems.AppletHelper;
 import panda.demo.Application;
 import panda.demo.entity.SampleDate;
@@ -12,8 +7,6 @@ import panda.demo.entity.SampleFile;
 import panda.demo.entity.SampleJoin;
 import panda.demo.entity.SampleTags;
 import panda.demo.entity.SampleUnique;
-import panda.log.Log;
-import panda.log.Logs;
 import panda.util.tool.AbstractCommandTool;
 
 
@@ -21,21 +14,7 @@ import panda.util.tool.AbstractCommandTool;
 /**
  */
 public class DropTables {
-	private static Log log = Logs.getLog(DropTables.class);
-
-	private Map<String, String> params = new HashMap<String, String>();
-	
 	public static class Main extends AbstractCommandTool {
-		@Override
-		protected void addCommandLineOptions() throws Exception {
-			super.addCommandLineOptions();
-		}
-
-		@Override
-		protected void getCommandLineOptions(CommandLine cl) throws Exception {
-			super.getCommandLineOptions(cl);
-		}
-
 		/**
 		 * main
 		 * @param args arguments
@@ -50,8 +29,6 @@ public class DropTables {
 	}
 
 	public void execute() throws Exception {
-		log.debug("params=" + params);
-		
 		AppletHelper.init(Application.class, false);
 		try {
 			AppletHelper.dropTables(
