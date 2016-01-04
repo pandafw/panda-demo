c11e9fa643ce65e314fc8c63de2898bb7964ebb7
Frank Wang <yf.frank.wang@outlook.com>
2016-01-04 16:51:12 +0900

    update

--- src/main/java/panda/demo/util/WebActionAssist.java
+++ src/main/java/panda/demo/util/WebActionAssist.java
@@ -3,17 +3,25 @@ package panda.demo.util;
 import panda.ioc.Scope;
 import panda.ioc.annotation.IocBean;
 import panda.mvc.util.ActionAssist;
+import panda.wing.constant.VC;
 import panda.wing.util.AppActionAssist;
 
 
 @IocBean(type=ActionAssist.class, scope=Scope.REQUEST)
 public class WebActionAssist extends AppActionAssist {
-	/**
-	 * Constructor
-	 */
+	@Override
 	public boolean isDebugEnabled() {
 		return isIntranetHost();
 	}
 
+	@Override
+	public long getLoginUserId() {
+		return VC.SYSTEM_UID;
+	}
+
+	@Override
+	public boolean hasPermission(String action) {
+		return true;
+	}
 
 }
