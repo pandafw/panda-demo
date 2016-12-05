69ee4e200ddbc18d095e786caad9783fb9d12dec
Frank Wang <yf.frank.wang@outlook.com>
2016-12-05 16:56:16 +0900

    add bloodTypes/booleans/countrycode consts

--- src/main/java/panda/demo/util/WebActionConsts.java
+++ src/main/java/panda/demo/util/WebActionConsts.java
@@ -1,5 +1,6 @@
 package panda.demo.util;
 
+import java.util.List;
 import java.util.Map;
 
 import panda.ioc.Scope;
@@ -25,4 +26,27 @@ public class WebActionConsts extends AppActionConsts {
 	public Map getStyleMap() {
 		return this.getTextAsMap("stylemap");
 	}
+
+
+	/**
+	 * @return map
+	 */
+	public Map getBooleanMap() {
+		return getTextAsMap("const-booleans");
+	}
+
+	/**
+	 * @return list
+	 */
+	public List getBloodTypeList() {
+		return getTextAsList("const-bloodTypes");
+	}
+	
+	/**
+	 * @return map
+	 */
+	public Map getGenderMap() {
+		return getTextAsMap("const-genders");
+	}
+	
 }
