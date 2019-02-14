b0b4215de2ec7e0732ec69a3a81587f84a8e8694
Frank Wang <yf.frank.wang@outlook.com>
2019-02-15 00:48:27 +0900

    fix bug for use inherited @Auth(super)

--- src/main/java/panda/demo/action/sample/PdfAction.java
+++ src/main/java/panda/demo/action/sample/PdfAction.java
@@ -1,8 +1,10 @@
 package panda.demo.action.sample;
 
-import panda.mvc.annotation.At;
 import panda.app.action.tool.Html2PdfAction;
+import panda.app.auth.Auth;
+import panda.mvc.annotation.At;
 
+@Auth
 @At("/pdf")
 public class PdfAction extends Html2PdfAction {
 
