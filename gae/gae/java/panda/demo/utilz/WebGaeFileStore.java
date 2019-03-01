03b875111aaef8bc2e99c14a69d812c10bc3e683
Frank Wang <yf.frank.wang@outlook.com>
2019-03-01 21:04:24 +0900

    add @IocBean for FileStore

--- gae/gae/java/panda/demo/utilz/WebGaeFileStore.java
+++ gae/gae/java/panda/demo/utilz/WebGaeFileStore.java
@@ -1,7 +1,10 @@
 package panda.demo.utilz;
 
+import panda.ioc.annotation.IocBean;
 import panda.mvc.vfs.MvcGaeFileStore;
+import panda.vfs.FileStore;
 
+@IocBean(type=FileStore.class)
 public class WebGaeFileStore extends MvcGaeFileStore {
 
 }
