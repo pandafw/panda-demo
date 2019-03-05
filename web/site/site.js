29150dcd7f52bfcb5be05c09e6500cfcc60cc5f2
Frank Wang <yf.frank.wang@outlook.com>
2019-03-05 22:39:26 +0900

    update maven property for log settings

--- web/site/site.js
+++ web/site/site.js
@@ -61,7 +61,7 @@ function s_setLang(v) {
 // google analytics
 //
 function s_google_analytics(c) {
-	if (c.google_analytics) {
+	if (c.google_analytics && c.google_analytics != "${GA}") {
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
