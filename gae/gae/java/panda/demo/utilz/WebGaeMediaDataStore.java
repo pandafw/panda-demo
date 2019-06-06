b6f8290a9b3f6cb47a6055b3d4fc1bc9c57e8d2e
Frank Wang <yf.frank.wang@outlook.com>
2019-06-06 22:35:48 +0900

    update for panda refactor

--- gae/gae/java/panda/demo/utilz/WebGaeMediaDataStore.java
+++ gae/gae/java/panda/demo/utilz/WebGaeMediaDataStore.java
@@ -1,7 +1,7 @@
 package panda.demo.utilz;
 
-import panda.app.media.MediaDataGaeStore;
-import panda.app.media.MediaDataStore;
+import panda.gems.media.store.MediaDataGaeStore;
+import panda.gems.media.store.MediaDataStore;
 import panda.ioc.annotation.IocBean;
 
 @IocBean(type=MediaDataStore.class)
